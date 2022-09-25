93a966abe369838aea173020d5a29cd0e7a41e93
AMOGH <amoghp549@gmail.com>
2022-09-25 23:42:35 +0530

    final mobile design.

--- app.js
+++ app.js
@@ -2,17 +2,47 @@ const cards = document.querySelectorAll('.card');
 const msg = document.querySelector('.msg');
 const hidden = document.querySelector('.hidden');
 const reds = document.querySelectorAll('.red-dot');
+const number = document.querySelector('.number');
+const mark = document.querySelector('.mark')
+
+
+
+let numbers = 0;
+for(let card of cards){
+    for(let red of reds){
+    if(card.contains(red)){
+        numbers++;
+    }
+}}
+number.innerText=numbers;
 
 
 for(let card of cards){
     card.addEventListener('click',function(){
+        for(let red of reds){
+            if(card.contains(red)){
+                if(red.classList.contains('hidden')===false){
+                    number.innerText--;
+                }
+                red.classList.add('hidden')
+            }}
     if(card.contains(msg)){
         msg.classList.toggle('hidden')
     }  
-    for(let red of reds){
-    if(card.contains(red)){
-        red.classList.add('hidden')
-    }}
-    })
+    
+})
     }
 
+
+mark.addEventListener('click',function(){
+    number.innerText=0;
+    for(let red of reds){
+    red.classList.add('hidden')
+    }
+})
+
+
+ 
+  
+
+    
